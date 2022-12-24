598ed310a3dc0a6a168f1da4565652a7926481eb
Dimitrios Iliadis <jimmyhliad@gmail.com>
2022-12-24 14:01:45 +0000

    small fix

--- Tutorial_1_DTI_Prediction_dot_product.py
+++ Tutorial_1_DTI_Prediction_dot_product.py
@@ -62,7 +62,7 @@ def main(num_samples, val_setting, cuda_id, num_workers, dataset_name, performan
     completed_param_combinations = {param_name: [] for param_name in ranges_dict.keys()}
     for run in tqdm(runs):
         if run.state == "finished":
-            if ((run.config['general_architecture_version'] == general_architecture_version) and (run.config['dataset_name'] == dataset_name)):
+            if ((run.config['general_architecture_version'] == general_architecture_version) and (run.config['dataset_name'] == dataset_name) and (run.config['val_setting'] == val_setting)):
                 for param_name in ranges_dict.keys():
                     if param_name == 'learning_rate':
                         completed_param_combinations[param_name].append(run.config['LR'])
