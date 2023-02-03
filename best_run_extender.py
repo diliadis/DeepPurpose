7ff03642b8ad0be6a879834cd564825cfd5ff697
Dimitrios Iliadis <jimmyhliad@gmail.com>
2023-02-03 21:16:28 +0000

    small update

--- best_run_extender.py
+++ best_run_extender.py
@@ -131,6 +131,7 @@ def main(cuda_id, num_workers, source_wandb_project_name, target_wandb_project_n
                                         wandb_dir = wandb_dir,
                                         num_workers=int(num_workers),
                                         parent_wandb_id = run.id,
+                                        dataset_name = dataset_name
                 )
                 
                 config.update({k: v for k,v in source_config.items() if k not in ['device', 'cuda_id', 'wandb_dir', 'num_workers', 'train_epoch', 'wandb_project_name']})
