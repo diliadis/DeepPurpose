7e6b186f61d91428e8a3c1bdff3a9344fdb80b60
Dimitrios Iliadis <jimmyhliad@gmail.com>
2023-03-19 17:12:33 +0000

    update

--- Tutorial_1_DTI_Prediction_dot_product.py
+++ Tutorial_1_DTI_Prediction_dot_product.py
@@ -124,7 +124,6 @@ def main(num_samples, val_setting, cuda_id, num_workers, dataset_name, performan
         print('testing the following config: '+str(temp_config))
         config = utils.generate_config(drug_encoding = drug_encoding, 
                                 target_encoding = target_encoding, 
-                                cls_hidden_dims = temp_config['cls_hidden_dims'], 
                                 train_epoch = 100, 
                                 LR = temp_config['learning_rate'], 
                                 batch_size = 256,
